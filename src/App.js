678d202bccab49fda6f0f4284682cdb7f7753e2b
PeterYan <yanlinchengrui@hotmail.com>
2018-08-14 22:13:27 -0700

    Updated it

--- src/App.js
+++ src/App.js
@@ -3,16 +3,81 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      objs : [ 'pho', 'ramen', 'diet'],
+      rand : ''
+    
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleRandom = this.handleRandom.bind(this);
+  }
+
+  handleAdd(event) {
+    let currObjs = this.state.objs;
+    let textBox = event.target.previousElementSibling;
+
+    if (textBox.value) {
+      currObjs.push(textBox.value);
+      textBox.value = '';
+      this.setState({
+        objs: currObjs
+      });
+    }
+  }
+
+  handleDelete(event) {
+    let currObj = event.target.textContent;
+    let updatedObjs = this.state.objs.filter((obj) => obj.trim() !== currObj.trim());
+    //const match = currObj.trim() == this.state.objs[0].trim();
+    //match ? alert("YEA") : alert("NO");
+    this.setState({
+      objs: updatedObjs
+    });
+  }
+
+  handleRandom() {
+    let currObjs = this.state.objs;
+    this.setState({
+      rand: currObjs[Math.floor(Math.random() * currObjs.length)]
+    });
+  }
+
   render() {
+
+    let objItem = this.state.objs.map( 
+      (obj, i) => {return <li className="Object" 
+                              onClick={this.handleDelete} 
+                              key = {i}> 
+                              {obj} 
+                          </li>}
+    );
+
     return (
-      <div className="App">
+      <div className="App">    
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"> Be a random person and do the right tings!</h1>
+          <p className="PS"> P.S. CLICK TO DELETE</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <input className="TextBox" type="text" />
+
+        <button className="Adding" onClick={this.handleAdd}>
+          add
+        </button>
+
+        <ul> {objItem} </ul>
+
+        <button className="Return" onClick={this.handleRandom}>
+          RETURN
+        </button>
+
+        <p className="Result"> {this.state.rand} </p>
       </div>
     );
   }
